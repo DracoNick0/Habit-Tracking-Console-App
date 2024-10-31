45270e9b1eeeab7d5bbaccefdc73c82900a14177
Nicholas Santos <santos_nick@outlook.com>
2024-10-31 00:07:59 -0700

    Added confirm input function in interface helper.

---
+++ Habit Tracking Console App/InterfaceHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace Habit_Tracking_Console_App
+{
+    class InterfaceHelper
+    {
+        public static bool ConfirmInput(string input)
+        {
+            while (true)
+            {
+                string? userInput = string.Empty;
+                Console.WriteLine("Is " + input + " correct? (y/n)");
+                userInput = Console.ReadLine();
+
+                if (userInput != null)
+                {
+                    userInput = userInput.ToLower();
+
+                    switch (userInput)
+                    {
+                        case "y":
+                            return true;
+                        case "n":
+                            return false;
+                        default:
+                            break;
+                    }
+                    return true;
+                }
+            }
+        }
+
+
+
+    }
+}
