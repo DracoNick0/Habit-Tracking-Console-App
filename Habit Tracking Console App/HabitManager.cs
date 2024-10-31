13e6f7a2fbbc91b8187307b9b78a78229fba2f8b
Nicholas Santos <santos_nick@outlook.com>
2024-10-30 23:40:13 -0700

    Habit manager now contains a dictionary of habits. A habit can also be added to the dictionary.
    Main program cannot execute this yet.

--- Habit Tracking Console App/HabitManager.cs
+++ Habit Tracking Console App/HabitManager.cs
@@ -2,11 +2,27 @@
 {
     class HabitManager
     {
-        public HabitManager() { }
+        Dictionary<string, HabitObject> habits;
 
-        public void AddHabit()
+        public HabitManager()
         {
-            Console.WriteLine("Add habit command invoked");
+            this.habits = new Dictionary<string, HabitObject>();
+        }
+
+        public bool AddHabit(HabitObject habit)
+        {
+            if (!this.habits.ContainsKey(habit.Habit))
+            {
+                this.habits.Add(habit.Habit, habit);
+                Console.WriteLine("Habit \"" + habit.Habit + "\" was added.");
+            }
+            else
+            {
+                Console.Error.WriteLine("A habit with the same name already exists, please try again.");
+            }
+
+
+            return false;
         }
     }
 }
