8c20db3dfe2aee3c810a21426dc483ff99262aa4
Nicholas Santos <santos_nick@outlook.com>
2024-10-30 22:44:45 -0700

    Created a class to handle the execution of commands. Execute command also determines whether the program keeps running.

---
+++ Habit Tracking Console App/CommandHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Habit_Tracking_Console_App
+{
+    class CommandHandler
+    {
+        public CommandHandler() { }
+
+        public static bool ExecuteCommand(string? userInput) {
+            if (userInput != null)
+            {
+                // Change user input to be digestable by the program
+                userInput = userInput.ToLower();
+
+                Console.Clear();
+                switch (userInput)
+                {
+                    case "help":
+                        Console.WriteLine("Commands:");
+                        Console.WriteLine(" - help: displays a list of commands for the user to input");
+                        Console.WriteLine(" - exit: exit the program");
+                        break;
+                    case "exit":
+                        Console.WriteLine("See you again!");
+                        return false;
+                    default:
+                        Console.Error.WriteLine("The command \"" + userInput + "\" is not valid, try again!");
+                        break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Habit Tracking Console App/Program.cs
+++ Habit Tracking Console App/Program.cs
@@ -1,33 +1,13 @@
-﻿Console.WriteLine("Welcome back!");
+﻿using Habit_Tracking_Console_App;
+
+Console.WriteLine("Welcome back!");
 
 
-bool wantsToExit = false;
 string? userInput = string.Empty;
 
-while (!wantsToExit)
-{
+do {
     // Wait for user input
     userInput = Console.ReadLine();
 
-    if (userInput != null)
-    {
-        // Change user input to be digestable by the program
-        userInput = userInput.ToLower();
-
-        Console.Clear();
-        switch (userInput)
-        {
-            case "help":
-                Console.WriteLine("Commands:");
-                Console.WriteLine(" - help: displays a list of commands for the user to input");
-                break;
-            case "exit":
-                Console.WriteLine("See you again!");
-                wantsToExit = true;
-                break;
-            default:
-                Console.Error.WriteLine("The command \"" + userInput + "\" is not valid, try again!");
-                break;
-        }
-    }
-}
+    // Execute the users input
+} while (CommandHandler.ExecuteCommand(userInput));
