22bb3344ec4f311b08cc37d062c2241735702186
Nicholas Santos <santos_nick@outlook.com>
2024-10-30 22:31:58 -0700

    Add project files.

---
+++ Habit Tracking Console App/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Welcome back!");
+
+
+bool notExit = true;
+string? userInput = string.Empty;
+while (notExit)
+{
+
+    userInput = Console.ReadLine();
+    if (userInput != null)
+    {
+        userInput = userInput.ToLower();
+
+        switch (userInput)
+        {
+            case "help":
+                Console.WriteLine("Commands:");
+                Console.WriteLine(" - help: displays a list of commands for the user to input");
+                break;
+            default:
+                Console.Error.WriteLine("That command was not valid, try again!");
+                break;
+        }
+    }
+}
