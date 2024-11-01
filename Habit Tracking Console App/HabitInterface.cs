43965c38af32d4314734c2f22c3c59b8a9f791a3
Nicholas Santos <santos_nick@outlook.com>
2024-11-01 13:53:20 -0700

    Added prompt for habit correction function that should request the user to make corrections to the habit they created.
    Should come after habit creation.

--- Habit Tracking Console App/HabitInterface.cs
+++ Habit Tracking Console App/HabitInterface.cs
@@ -26,9 +26,50 @@ namespace Habit_Tracking_Console_App
             isGood = PromptForIsGood();
             importance = PromptForImportance();
 
+            this.PromptForHabitCorrection(ref name, ref description, ref isGood, ref importance);
+
             this.habitManager.AddHabit(new HabitObject(name, isGood, description, importance));
 
             return true;
+        }
+
+        private bool PromptForHabitCorrection(ref string name, ref string description, ref bool isGood, ref int importance)
+        {
+            string? userInput;
+
+            string prompt = "If a detail is incorrect, type it's name to change the property, otherwise press enter.\n";
+            prompt += "Habit details:\n";
+            prompt += " - Name: " + name + "\n";
+            prompt += " - Desc: " + description + "\n";
+            prompt += " - IsGood: " + isGood + "\n";
+            prompt += " - Importance: " + importance + "\n";
+
+            while (true)
+            {
+                userInput = InterfaceHelper.PromptForNotNullInput(prompt);
+                userInput.ToLower();
+
+                switch (userInput)
+                {
+                    case "name":
+                        name = PromptForName();
+                        break;
+                    case "desc":
+                        description = PromptForDescription();
+                        break;
+                    case "isgood":
+                        isGood = PromptForIsGood();
+                        break;
+                    case "importance":
+                        importance = PromptForImportance();
+                        break;
+                    case "":
+                        return true;
+                    default:
+                        Console.Error.WriteLine("Input was not valid, try again!");
+                        break;
+                }
+            }
         }
 
         private string PromptForName()
@@ -45,7 +86,7 @@ namespace Habit_Tracking_Console_App
 
         private bool PromptForIsGood()
         {
-            string isGoodPrompt = "Is the habit a good habit? ";
+            string isGoodPrompt = "Is this a good habit, enter true or false: ";
             return InterfaceHelper.PromptForTrueFalseInput(isGoodPrompt);
         }
 
@@ -53,7 +94,7 @@ namespace Habit_Tracking_Console_App
         {
             int? importance = null;
 
-            string importancePrompt = "If 1 is trivial and 5 is of utmost importance, enter the digit that represents the importance of this habit: ";
+            string importancePrompt = "If 1 is trivial and 5 is of utmost importance, enter the digit that represents the habits importance: ";
             while ((importance = InterfaceHelper.PromptForIntInput(importancePrompt)) == null || 1 > importance || importance > 5)
             {
                 Console.Clear();
