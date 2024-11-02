b586a161bd5272c53a54b1f560685e710b08f191
Nicholas Santos <santos_nick@outlook.com>
2024-11-01 21:22:18 -0700

    Created an action logger that will help print the screen after the user resizes the console.

---
+++ Habit Tracking Console App/Backend/ActionLogger.cs
@@ -0,0 +1,25 @@
+﻿namespace Habit_Tracking_Console_App.Backend
+{
+    class ActionLogger
+    {
+        private static List<Action> previousActions = new List<Action>();
+
+        public static void AddAction(Action action)
+        {
+            previousActions.Add(action);
+        }
+
+        public static void ExecuteStoredActions()
+        {
+            foreach (Action action in previousActions)
+            {
+                action.Invoke();
+            }
+        }
+
+        public static void ClearStoredActions()
+        {
+            previousActions.Clear();
+        }
+    }
+}
