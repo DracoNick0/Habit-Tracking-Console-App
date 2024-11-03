0f5e8195a3a298da6ba4da05d6aa80b30ae2df69
Nicholas Santos <santos_nick@outlook.com>
2024-11-02 17:02:56 -0700

    ConsoleResizeMonitor comment blocks.

--- Habit Tracking Console App/Model/ConsoleResizeMonitor.cs
+++ Habit Tracking Console App/Model/ConsoleResizeMonitor.cs
@@ -1,14 +1,23 @@
 ﻿namespace Habit_Tracking_Console_App.Model
 {
+    /// <summary>
+    /// Monitors the console window size.
+    /// </summary>
     class ConsoleResizeMonitor
     {
         private static bool running = true;
 
+        /// <summary>
+        /// Creates a thread that runs parallel to the program.
+        /// </summary>
         public ConsoleResizeMonitor()
         {
             Task.Run(() => MonitorConsoleResize());
         }
 
+        /// <summary>
+        /// Continuously checks if the size of the window has changed, and if it does, it executes all stored actions.
+        /// </summary>
         private void MonitorConsoleResize()
         {
             int previousWidth = Console.WindowWidth;
