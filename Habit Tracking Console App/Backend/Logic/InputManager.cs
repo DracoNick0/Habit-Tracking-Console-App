a4213c1d5905c463ae4579f1aec8fe5af98c0995
Nicholas Santos <santos_nick@outlook.com>
2024-11-04 12:56:08 -0800

    Setting up InputManager to use IO.

--- Habit Tracking Console App/Backend/Logic/InputManager.cs
+++ Habit Tracking Console App/Backend/Logic/InputManager.cs
@@ -1,6 +1,27 @@
-﻿namespace Habit_Tracking_Console_App.Backend.Logic
+﻿using Habit_Tracking_Console_App.Backend.Objects;
+
+namespace Habit_Tracking_Console_App.Backend.Logic
 {
     class InputManager
     {
+        public static bool GetBoolInput(params string[] prompt)
+        {
+            return false;
+        }
+
+        public static int GetIntegerInput(params string[] prompt)
+        {
+            return 0;
+        }
+
+        public static DateTime GetDateInput(params string[] prompt)
+        {
+            return DateTime.Now;
+        }
+
+        public static RecurrenceEnum GetRecurrenceInput(params string[] prompt)
+        {
+            return RecurrenceEnum.none;
+        }
     }
 }
